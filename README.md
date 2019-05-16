e02bc7752ea1c7c781b9313dbaa55d09d0d3c6af
atka66 <atkavagyok@gmail.com>
2019-05-16 20:53:08 +0200

    ugly baby steps

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 * Dying causes you to lose all your items, exp and warp points (only your levels and stats stay)
 * Armors: Helmet, Armor, Gloves, Boots
 * Weapons: Weapons, Skills, Spells
-* Sets: Equiping more items from the same set gives unique bonuses
+* Sets: Equipping more items from the same set gives unique bonuses
 * Item trading with vendors or other players
 * Attributes: STR - AGI - INT
 * STR: More HP + More DMG
--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('Dongon', []);
+var app = angular.module('Dongon', [])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'login/login-tpl.html',
+				controller: 'LoginCtrl'
+			})
+			.otherwise({
+						   redirectTo: '/'
+					   });
+	})
+	.config(['growlProvider', function(growlProvider) {
+		growlProvider.globalDisableIcons(true);
+		growlProvider.globalDisableCloseButton(true);
+		growlProvider.globalDisableCountDown(true);
+		growlProvider.globalTimeToLive(3000);
+	}]);
