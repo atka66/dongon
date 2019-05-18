26cc91d498d3edd01171ccddb70f74c1ae8ac463
atka66 <atkavagyok@gmail.com>
2019-05-18 10:49:53 +0200

    giant baby steps

---
+++ dongon-backend/src/main/java/hu/atka/dongon/DongonBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.atka.dongon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongonBackendApplication.class, args);
+	}
+
+}
