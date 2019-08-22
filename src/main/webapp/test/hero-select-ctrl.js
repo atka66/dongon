d42e44cd3bf923ee1cd52300f51661eda7dcbceb
atka66 <atkavagyok@gmail.com>
2019-08-22 21:28:55 +0200

    hero selection screen modified + added correct screen meta

--- src/main/webapp/test/hero-select-ctrl.js
+++ src/main/webapp/test/hero-select-ctrl.js
@@ -6,7 +6,7 @@ app.controller('HeroSelectCtrl', function ($scope) {
 
 	$scope.characters = [
 	{'name': 'Atka', 'lvl': 11, 'gold': 55, 'maxLvl': 13, 'deaths': 3},
-	{'name': 'Batka', 'lvl': 84, 'gold': 987242, 'maxLvl': 125, 'deaths': 12}
+	{'name': 'Batka', 'lvl': 84, 'gold': 98724, 'maxLvl': 125, 'deaths': 12}
 	];
 
 	$scope.selected = $scope.characters[0];
